6348cd551958b27b65a77d5e3699f428b476b10a
a1033899938 <a1033899938@gmail.com>
2025-09-06 12:39:51 +0800

    h5 to txt-v2

--- src/general/load_data/auto_h5_to_txt.py
+++ src/general/load_data/auto_h5_to_txt.py
@@ -15,7 +15,6 @@ def h5_to_txt(h5_filepath, des_folder):
     :param des_folder:
     :return:
     """
-    """暂时只能读取一维数据，读取时间序列数据部分待完成"""
     def traversal_all_level(f, folder_now):
         for key in f.keys():
             print(key, type(f[key]))
@@ -27,26 +26,55 @@ def h5_to_txt(h5_filepath, des_folder):
                 try:
                     data = f[key]
                     if "integration_time" in data.attrs:  # 视为光谱文件
-                        sp = np.array(data)
-                        sp_time = np.array(data.attrs.get('integration_time', []))
-                        sp = sp / sp_time
+                        if "number of spectra" in data.attrs:  # 视为时间序列光谱
+                            sp_num = data.attrs.get('number of spectra')
 
-                        bgd = np.array(data.attrs.get('background', []))
-                        bgd_time = np.array(data.attrs.get('background_int', [])) / 1000
-                        bgd = bgd / bgd_time
+                            sp = np.array(data)
+                            sp_time = np.array(data.attrs.get('integration_time', [])) / 1000
+                            sp = sp / sp_time
+                            print(np.shape(sp[0]))
 
-                        ref = np.array(data.attrs.get('reference', []))
-                        ref_time = np.array(data.attrs.get('reference_int', [])) /1000
-                        ref = ref / ref_time
+                            bgd = np.array(data.attrs.get('background', []))
+                            bgd_time = np.array(data.attrs.get('background_int', [])) / 1000
+                            bgd = bgd / bgd_time
 
-                        wav = np.array(data.attrs.get('wavelengths', []))
+                            ref = np.array(data.attrs.get('reference', []))
+                            ref_time = np.array(data.attrs.get('reference_int', [])) /1000
+                            ref = ref / ref_time
 
-                        datas = [sp, bgd, ref, wav]
-                        lines_names = ["sp", "bgd", "ref", "wav"]
-                        filtered = [(d, n) for d, n in zip(datas, lines_names) if d.size > 0]
-                        datas, lines_names = zip(*filtered) if filtered else ([], [])
-                        save_path = os.path.join(folder_now, f"{key}.txt")
-                        save_lines_txt(*datas, save_path=save_path, separator= ' ', lines_names=lines_names, confirm_all_overwrite=False)
+                            wav = np.array(data.attrs.get('wavelengths', []))
+
+                            sps = [sp[i] for i in range(sp_num)]
+                            sps_names = [f"sp-{i}" for i in range(sp_num)]
+
+                            datas = [*sps, bgd, ref, wav]
+                            lines_names = [*sps_names, "bgd", "ref", "wav"]
+                            filtered = [(d, n) for d, n in zip(datas, lines_names) if d.size > 0]
+                            datas, lines_names = zip(*filtered) if filtered else ([], [])
+                            save_path = os.path.join(folder_now, f"{key}.txt")
+                            save_lines_txt(*datas, save_path=save_path, separator= ' ', lines_names=lines_names, confirm_all_overwrite=False)
+                        else:  # 视为单张光谱
+                            sp = np.array(data)
+                            sp_time = np.array(data.attrs.get('integration_time', []))
+                            sp = sp / sp_time
+
+                            bgd = np.array(data.attrs.get('background', []))
+                            bgd_time = np.array(data.attrs.get('background_int', [])) / 1000
+                            bgd = bgd / bgd_time
+
+                            ref = np.array(data.attrs.get('reference', []))
+                            ref_time = np.array(data.attrs.get('reference_int', [])) / 1000
+                            ref = ref / ref_time
+
+                            wav = np.array(data.attrs.get('wavelengths', []))
+
+                            datas = [sp, bgd, ref, wav]
+                            lines_names = ["sp", "bgd", "ref", "wav"]
+                            filtered = [(d, n) for d, n in zip(datas, lines_names) if d.size > 0]
+                            datas, lines_names = zip(*filtered) if filtered else ([], [])
+                            save_path = os.path.join(folder_now, f"{key}.txt")
+                            save_lines_txt(*datas, save_path=save_path, separator=' ', lines_names=lines_names,
+                                           confirm_all_overwrite=False)
                     elif "image_size" in data.attrs:  # 视为图像
                         img = np.asarray(data)
                         save_path = os.path.join(folder_now, f"{key}.png")
@@ -59,7 +87,6 @@ def h5_to_txt(h5_filepath, des_folder):
                     print(e)
 
     with h5py.File(h5_filepath, 'r') as f:
-        # f = f["LumeneraCamera"]
         traversal_all_level(f, des_folder)
 
 if __name__ == "__main__":
